a4c53d679252eed4a1a3636395788120be3bda2b
LawrenceHwang <CoffeePoweredLion@gmail.com>
2016-01-04 12:59:42 -0800

    Create Start-Pomodoro.ps1

---
+++ Start-Pomodoro.ps1
@@ -0,0 +1,110 @@
+<#
+.Synopsis
+   A simple Pomodoro clock with PowerShell
+.DESCRIPTION
+   The script will act as a Pomodoro clock for those who use the time management technique. The default is 25 minute Pomorodo work time followed by 5 minutes break.
+   PowerShell progress bar will display the current status. Also you can use -EnableAudio to have it beep at the end of 25 minutes.
+   You can specify the number of Pomodomos as well.
+.EXAMPLE
+   Start-Podomoro -PomodoroCount 3 -Verbose
+.EXAMPLE
+   Start-Podomoro -PomodoroCount 3 -EnableAudio -Verbose
+.NOTES
+   Time: 2016-Jan-04
+   Created by Lawrence Hwang
+   Twitter: CPoweredLion
+#>
+
+
+function Start-Podomoro {
+
+    [cmdletBinding()]
+
+    Param(
+        
+        [Parameter(Mandatory=$false,
+                   HelpMessage ="Pomodoro Count",
+                   Position=0)]    
+        [int]$PomodoroCount = 1,
+
+        [Parameter(Mandatory=$false,
+                   HelpMessage ="Time (minutes) in each Pomodoro",
+                   Position=1)]
+        [int]$OnePomodoroTime =25,
+
+        [Parameter(Mandatory=$false,
+                   HelpMessage ="Break time (minutes) in each Pomodoro")]
+        [int]$BreakTime =5,
+        
+        [Parameter(Mandatory=$false,
+                   HelpMessage ="Second in every minute. Used for testing purpose.")]
+        [int] $second =60,
+
+        [Parameter(Mandatory=$false,
+                   HelpMessage ="Enable the beep")]  
+        [switch] $EnableAudio
+    
+    )
+    
+    Begin {
+
+        Clear-host
+        Write-Verbose -Message "Pomodoro Clock started on $((get-date).toshorttimestring())"
+    
+    }
+
+    Process{
+        
+        $p = 0
+
+        Do{
+            For ($i=0 ; $i -lt $OnePomodoroTime ; $i ++){
+            
+                Write-Progress -Activity 'Pomodoro Clock' -PercentComplete ($i/$OnePomodoroTime*100) -Status 'Minutes'
+
+                For ($j=1 ; $j -lt 2 ; $j ++){                 
+            
+                        Write-Progress -id 1 -Activity 'Tick Tock' -PercentComplete ($j/$second*100) -Status 'Seconds'
+                        Start-Sleep -Seconds 1
+            
+                }
+            }
+
+            if ($EnableAudio){
+                    
+                    #Beep
+                    1..3 | ForEach-Object { [System.Media.SystemSounds]::beep.Play() ; sleep -m 470}
+                    
+            }
+             
+            #Provide visual feedback with pop-up windows. The windows will be in focus.
+            [void] [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.VisualBasic")
+            [Microsoft.VisualBasic.Interaction]::MsgBox("Good Job! $($p+1) Pomodoro ($(($p+1)*$OnePomodoroTime) min) of $PomodoroCount Pomodoro completed. Press OK to start 5 min break.", "OKOnly,SystemModal,Information", "Pomodoro Clock") | out-null
+
+            For ($b=1 ; $b -lt $BreakTime*$second ; $b ++){                 
+            
+                Write-Progress -id 2 -Activity '5 Min Break' -PercentComplete ($b/($BreakTime*$second)*100) -Status "Don`'t look at me!"
+                Start-Sleep -Seconds 1
+                    
+            }
+
+            if ($EnableAudio){
+
+                #Beep
+                1..3 | ForEach-Object { [System.Media.SystemSounds]::beep.Play() ; sleep -m 470}
+                    
+            }
+            
+            #Provide visual feedback with pop-up windows. The windows will be in focus.
+            [Microsoft.VisualBasic.Interaction]::MsgBox("Break done for $($p+1) of $PomodoroCount Pomodoro.Press OK to continue.", "OKOnly,SystemModal,Information", "Pomodoro Clock") | out-null
+
+            $p = $p + 1
+
+        } While ($p -lt $PomodoroCount )    
+    }
+
+    End{
+        Write-Verbose -Message "Completed $PomodoroCount Pomodoro."
+        Write-Verbose -Message "Pomodoro Clock ended on $((get-date).toshorttimestring())"
+    }
+}
